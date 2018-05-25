820d300f3fcad8f8a91ae10608b89f058d41215b
Jacob Horstmeier <37892205+Dafluffinmuffin@users.noreply.github.com>
2018-05-25 11:12:37 -0600

    first commit

---
+++ shelfie/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Dashboard from './component/Dashboard/Dashboard';
+import Form from './component/Form/Form';
+import Header from './component/Header/Header';
+import Product from './component/Product/Product';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Dashboard />
+        <Form />
+        <Header />
+        <Product />
+      </div>
+    );
+  }
+}
+
+export default App;
